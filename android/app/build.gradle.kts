066bc7207110aa10398e06322ec062389e83aec8
keep_wan <whevether@outlook.com>
2025-12-06 21:32:57 +0800

    update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,40 +1,92 @@
+import java.io.FileInputStream
+import java.util.Properties
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
+val localProperties = Properties()
 
+// 加载打包key
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    localProperties.load(FileInputStream(keystorePropertiesFile))
+}
 android {
     namespace = "com.example.flutter_riverpod_template"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 36
+    ndkVersion = "29.0.14206865"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+    kotlin {
+      compilerOptions {
+        jvmTarget = JvmTarget.JVM_17
+      }
     }
-
+    packaging {
+        dex {
+            useLegacyPackaging = true
+        }
+        jniLibs {
+            useLegacyPackaging = true
+        }
+    }
+    packaging {
+        resources {
+            excludes += "lib/**/libc++_shared.so"
+        }
+    }
+    // externalNativeBuild {
+    //     cmake {
+    //         path = rootProject.file("whisper_ggml/android/src/whisper/CMakeLists.txt")
+    //         version = "3.22.1"
+    //     }
+    // }
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.flutter_riverpod_template"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 24
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        multiDexEnabled = true
+        ndk {
+            abiFilters += listOf("armeabi-v7a","arm64-v8a", "x86_64")
+        }
     }
-
+    signingConfigs {
+        create("release") {
+            keyAlias = localProperties["keyAlias"] as String
+            keyPassword = localProperties["keyPassword"] as String
+            storeFile = localProperties["storeFile"]?.let { file(it as String) }
+            storePassword = localProperties["storePassword"] as String
+            // isMinifyEnabled = true
+            // isShrinkResources = false
+            // enableProguard = true
+        }
+    }
     buildTypes {
+       debug {
+            signingConfig = signingConfigs.getByName("debug")
+        }
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            // isMinifyEnabled = true
+            // isShrinkResources = true
+            signingConfig = signingConfigs.getByName("release")
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            isMinifyEnabled = true
+            isShrinkResources = true
         }
     }
 }
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -2,6 +2,8 @@ allprojects {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+        maven { url  = uri("https://maven.aliyun.com/repository/public") }
     }
 }
 
@@ -9,8 +11,26 @@ val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build"
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
+    afterEvaluate {
+        if (plugins.hasPlugin("com.android.application") || plugins.hasPlugin("com.android.library")) {
+            extensions.configure<com.android.build.gradle.BaseExtension> {
+                compileSdkVersion(36)
+                buildToolsVersion = "36.0.0"
+                ndkVersion = "29.0.14206865" 
+                if (namespace == null) {
+                    namespace = project.group.toString()
+                }
+            }
+        }
+    }
+    layout.buildDirectory.set(rootProject.layout.buildDirectory.dir(project.name))
+    dependencyLocking {
+        ignoredDependencies.add("io.flutter:*")
+        lockFile = file("${rootProject.projectDir}/project-${project.name}.lockfile")
+        if (!project.hasProperty("local-engine-repo")) {
+            lockAllConfigurations()
+        }
+    }
 }
 subprojects {
     project.evaluationDependsOn(":app")
