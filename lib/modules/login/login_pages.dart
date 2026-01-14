c4f238e83f95563aaae1126b72e848313779b8ae
keep_wan <whevether@outlook.com>
2026-01-14 21:00:51 +0800

    Update login_pages.dart

--- lib/modules/login/login_pages.dart
+++ lib/modules/login/login_pages.dart
@@ -3,8 +3,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod_template/app/base/base_consumer_stateful_widget.dart';
 import 'package:flutter_riverpod_template/app/base/base_state.dart';
 import 'package:flutter_riverpod_template/router/router_path.dart';
-import 'package:flutter_riverpod_template/services/user_service.dart';
-import 'package:go_router/go_router.dart';
 
 class LoginPages extends BaseConsumerStatefulWidget {
   const LoginPages({super.key});
@@ -17,16 +15,6 @@ class LoginPages extends BaseConsumerStatefulWidget {
 class _LoginPageState extends BaseConsumerState<LoginPages> {
   @override
   Widget build(BuildContext context) {
-    //监听登录状态改变来处理跳转到首页
-    ref.listen<AsyncValue<UserState>>(userProvider, (previous, next) {
-      next.whenOrNull(
-        data: (user) {
-          if (user.loginResult != null) {
-            context.go(RoutePath.kIndex);
-          }
-        },
-      );
-    });
     return Scaffold(
       appBar: AppBar(title: const Text('Login Page')),
       body: ListView(
