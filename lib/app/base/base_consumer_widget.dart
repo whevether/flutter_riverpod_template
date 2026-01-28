05709fbdb747fc6e8262139edd036586d327c435
keep_wan <whevether@outlook.com>
2026-01-28 09:46:56 +0800

    Add UIWidget mixin to BaseConsumerWidget

--- lib/app/base/base_consumer_widget.dart
+++ lib/app/base/base_consumer_widget.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod_template/app/base/ui_adapter.dart';
 
-abstract class BaseConsumerWidget extends ConsumerWidget with UIAdapter {
+abstract class BaseConsumerWidget extends ConsumerWidget with UIAdapter,UIWidget {
   const BaseConsumerWidget({super.key});
 }
