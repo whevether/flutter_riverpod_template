342945e26672b078738bf7e02d7bf1740d9b21ba
keep_wan <whevether@outlook.com>
2026-05-06 22:22:11 +0800

    Update base_stateful_widget.dart

--- lib/app/base/base_stateful_widget.dart
+++ lib/app/base/base_stateful_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod_template/app/base/ui_adapter.dart';
+import 'package:flutter_riverpod_template/app/base/ui_widget.dart';
 
-abstract class BaseStatefulWidget extends StatefulWidget with UIAdapter {
+abstract class BaseStatefulWidget extends StatefulWidget with UIAdapter,UIWidget {
   const BaseStatefulWidget({super.key});
 }
