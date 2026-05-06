0489fe9c63489d7fc37eeee794df3f437ec801f4
keep_wan <whevether@outlook.com>
2026-05-06 22:23:20 +0800

    Update base_consumer_stateful_widget.dart

--- lib/app/base/base_consumer_stateful_widget.dart
+++ lib/app/base/base_consumer_stateful_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod_template/app/base/ui_adapter.dart';
+import 'package:flutter_riverpod_template/app/base/ui_widget.dart';
 
-abstract class BaseConsumerStatefulWidget extends ConsumerStatefulWidget with UIAdapter {
+abstract class BaseConsumerStatefulWidget extends ConsumerStatefulWidget with UIAdapter,UIWidget {
   const BaseConsumerStatefulWidget({super.key});
 }
