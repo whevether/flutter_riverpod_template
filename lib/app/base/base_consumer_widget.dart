f92287307a94acb9ddfd53dccb994c5c65144e1d
keep_wan <whevether@outlook.com>
2026-05-06 22:22:24 +0800

    Update base_consumer_widget.dart

--- lib/app/base/base_consumer_widget.dart
+++ lib/app/base/base_consumer_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod_template/app/base/ui_adapter.dart';
+import 'package:flutter_riverpod_template/app/base/ui_widget.dart';
 
 abstract class BaseConsumerWidget extends ConsumerWidget with UIAdapter,UIWidget {
   const BaseConsumerWidget({super.key});
